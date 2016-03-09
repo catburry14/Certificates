601420c9e4fd504a201b0e696309917d75225b3f
catburry14 <nishikanthkota@gmail.com>
2016-03-08 22:36:22 -0500

    ll

--- Certificates/WebContent/javascript/controllers/Login.js
+++ Certificates/WebContent/javascript/controllers/Login.js
@@ -1,3 +1,18 @@
 mainApp.controller('LoginController', function($scope) {
 	
+	    $scope.userName = "";
+	    $scope.passWord = "";
+	    $scope.invalidUNandPW = "valid";
+	    
+	    $scope.apply();
+	    
+	    $scope.submit = function(){
+	    	if(userName == "root" && passWord == "tiger")
+	    		$location.href = '/Home';
+	    	else{
+	    		invalidUNandPW = "invalid";
+	    		$scope.apply();
+	    	}
+	    }
+	
 });
